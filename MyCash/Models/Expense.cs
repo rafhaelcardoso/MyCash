6f263907503b4932cedc1b485a54d3222761d2f5
rafhaelcardoso <rafhaelcardosos@gmail.com>
2022-10-08 14:30:00 -0300

    Add Expense and Income Classes to project

---
+++ MyCash/Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCash.Models
+{
+    public class Expense
+    {
+        [Key]
+        [Required]
+        public int ExpenseId { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        [Display(Name = "Descrição")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        [Display(Name = "Valor")]
+        public decimal Amount { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        [Display(Name = "Data")]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ MyCash/Models/Income.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCash.Models
+{
+    public class Income
+    {
+        [Key]
+        [Required]
+        public int IncomeId { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        [Display(Name = "Descrição")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        [Display(Name = "Valor")]
+        public decimal Amount { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        [Display(Name = "Data")]
+        public DateTime Date { get; set; }
+    }
+}
